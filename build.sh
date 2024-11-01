08b16770158fc2dac66c42f418baa5472ebb3445
WangYan <me@wangyan.org>
2024-11-02 02:50:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ if [ "$1" = "dev" ]; then
   version="dev"
   webVersion="dev"
 else
+  git tag -d beta
   version=$(git describe --abbrev=0 --tags)
   webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/wangyan/alist-web-dist/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
@@ -122,6 +123,8 @@ elif [ "$1" = "release" ]; then
     BuildRelease
     MakeRelease "md5.txt"
   fi
+elif [ "$1" = "zip" ]; then
+  MakeRelease "$2".txt
 else
   echo -e "Parameter error"
 fi
